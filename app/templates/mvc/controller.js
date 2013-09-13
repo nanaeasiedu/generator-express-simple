fb289b44d37beb50e2e03b0e1d2e667f5e728be1
Eugene Asiedu <ngene84@gmail.com>
2013-09-13 12:45:17 +0000

    wow some typos here and there

--- app/templates/mvc/controller.js
+++ app/templates/mvc/controller.js
@@ -13,7 +13,7 @@ exports.index = module.exports = function (req, res) {
   });
 };<% } else if (db === 'sequelize') { %>
 exports.index = module.exports = function (req, res) {
-  Items.all().success(function (items) {
+  Items.all().success(function (itemsList) {
     res.render(<% if (htmlEngine === 'jade') { %>'index'<% } else if (htmlEngine === 'haml') { %>'index.haml'<% } else if (htmlEngine === 'underscore') { %>'index.html'<% } %>, {
       title: 'Express-simple',
       items: itemsList
