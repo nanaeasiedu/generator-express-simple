d85b56c4eea4ad7d53b295c2a4f53a17b527f815
Eugene Asiedu <ngene84@gmail.com>
2014-04-09 13:57:22 +0000

    add the necessary tasks for dev

--- app/templates/gulpfiles/gulpfile.coffee
+++ app/templates/gulpfiles/gulpfile.coffee
@@ -2,11 +2,17 @@ gulp = require 'gulp'
 <%= cssPreprocessor %> = require 'gulp-<%= cssPreprocessor %>'
 concat = require 'gulp-concat'
 jshint = require 'gulp-jshint'
+uglify = require 'gulp-uglify'
+watch = require 'gulp-watch'
+minifyCss = require 'gulp-minify-css'
+rename = require 'gulp-rename'
+concat =  require 'gulp-concat'
+livereload = require 'gulp-livereload'
 nodemon = require 'gulp-nodemon'
 
-paths:
+paths =
   server: [<% if (options.mvc) { %>'controllers/**/*.js', 'models/**/*.js', <% } %>'routes/**/*.js', 'app.js', 'config.js']
-  client: 'public/js/**/*.js'
+  client: ['./public/js/**/*.js', '!./public/js/**/*.min.js']
 
 gulp.task 'lintserver', ->
   gulp
@@ -20,7 +26,18 @@ gulp.task 'lintclient', ->
     .pipe jshint '.jshintrc'
     .pipe jshint.reporter 'jshint-stylish'
 
-gulp.task 'lint', ['lintserver', 'lintclient']
+gulp.task 'uglify', ->
+  gulp
+    .src paths.client
+    .pipe uglify()
+    .pipe rename suffix: '.min'
+    .pipe gulp.dest './public/js'
+
+gulp.task 'concatJs', ->
+  gulp
+    .src ['./public/vendor/jquery/dist/jquery.min.js', './public/vendor/bootstrap/dist/js/bootstrap.min.js', './public/js/**/*.min.js']
+    .pipe concat 'app.min.js'
+    .pipe gulp.dest './public/js'
 
 gulp.task '<%= cssPreprocessor %>', ->
   gulp
@@ -28,7 +45,20 @@ gulp.task '<%= cssPreprocessor %>', ->
     .pipe <%= cssPreprocessor %>()
     .pipe gulp.dest './public/css'
 
-gulp.task 'dev'. ->
+gulp.task 'css', ->
+  gulp
+    .src ['./public/css/**/*.css', '!./public/css/**/*.min.css']
+    .pipe minifyCss()
+    .pipe rename suffix: '.min'
+    .pipe gulp.dest './public/css'
+
+gulp.task 'concatCss', ->
+  gulp
+    .src ['./public/vendor/bootstrap/dist/css/bootstrap.min.css,' './public/css/**/*.min.css']
+    .pipe concat 'app.styles.min.css'
+    .pipe gulp.dest './public/css'
+
+gulp.task 'dev', ->
   nodemon
     script: 'app.js'
     nodeArgs: ['--debug']
@@ -38,3 +68,20 @@ gulp.task 'dev'. ->
     watch: paths.server
     delay: 1
     legacyWatch: true
+
+gulp.task 'watch', ->
+  server = livereload()
+
+  gulp.watch paths.server, ['lintserver']
+  gulp.watch paths.client, ['lintclient']
+  gulp.watch paths.client, ['buildJs']
+  gulp.watch './public/<%= cssPreprocessor %>/**/*.<%= cssExt %>', ['buildCss']
+  gulp
+    .src ['./views/**/*.<%= viewEngine %>', './public/css/**/*.min.css', './public/js/**/*.min.js']
+    .pipe watch()
+    .pipe server
+
+gulp.task 'lint' ['lintserver', 'lintclient']
+gulp.task 'buildCss' ['<%= cssPreprocessor %>', 'css', 'concatCss']
+gulp.task 'buildJs' ['uglifyJs', 'concatJs']
+gulp.task 'default', ['lint', 'buildCss', 'buildJs', 'watch']
--- app/templates/gulpfiles/gulpfile.js
+++ app/templates/gulpfiles/gulpfile.js
@@ -2,11 +2,17 @@ var gulp = require('gulp'),
     <%= cssPreprocessor %> = require('gulp-<%= cssPreprocessor %>'),
     concat = require('gulp-concat'),
     jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    watch = require('gulp-watch'),
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    concat =  require('gulp-concat'),
+    livereload = require('gulp-livereload'),
     nodemon = require('gulp-nodemon');
 
 var paths = {
   server: [<% if (options.mvc) { %>'controllers/**/*.js', 'models/**/*.js', <% } %>'routes/**/*.js', 'app.js', 'config.js'],
-  client: 'public/js/**/*.js'
+  client: ['./public/js/**/*.js', '!./public/js/**/*.min.js']
 };
 
 gulp.task('lintserver', function () {
@@ -18,17 +24,45 @@ gulp.task('lintserver', function () {
 
 gulp.task('lintclient', function () {
   gulp
-    .src(paths.server)
+    .src(paths.client)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('lint', ['lintserver', 'lintclient']);
+gulp.task('uglify', function () {
+  gulp
+    .src(paths.client)
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./public/js'))
+});
+
+gulp.task('concatJs', function () {
+  gulp
+    .src(['./public/vendor/jquery/dist/jquery.min.js', './public/vendor/bootstrap/dist/js/bootstrap.min.js', './public/js/**/*.min.js'])
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('./public/js'));
+});
 
 gulp.task('<%= cssPreprocessor %>', function () {
   gulp
     .src('./public/<%= cssPreprocessor %>/**/*.<%= cssExt %>')
     .pipe(<%= cssPreprocessor %>())
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('css', function () {
+  gulp
+    .src(['./public/css/**/*.css', '!./public/css/**/*.min.css'])
+    .pipe(minifyCss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('concatCss', function () {
+  gulp
+    .src(['./public/vendor/bootstrap/dist/css/bootstrap.min.css', './public/css/**/*.min.css'])
+    .pipe(concat('app.styles.min.css'))
     .pipe(gulp.dest('./public/css'));
 });
 
@@ -44,3 +78,21 @@ gulp.task('dev', function () {
     legacyWatch: true
   });
 });
+
+gulp.task('watch', function () {
+  var server = livereload();
+
+  gulp.watch(paths.server, ['lintserver']);
+  gulp.watch(paths.client, ['lintclient']);
+  gulp.watch(paths.client, ['buildJs']);
+  gulp.watch('./public/<%= cssPreprocessor %>/**/*.<%= cssExt %>', ['buildCss']);
+  gulp
+    .src(['./views/**/*.<%= viewEngine %>', './public/css/**/*.min.css', './public/js/**/*.min.js'])
+    .pipe(watch())
+    .pipe(server);
+});
+
+gulp.task('lint', ['lintserver', 'lintclient']);
+gulp.task('buildCss', ['<%= cssPreprocessor %>', 'css', 'concatCss']);
+gulp.task('buildJs', ['uglifyJs', 'concatJs']);
+gulp.task('default', ['lint', 'buildCss', 'buildJs', 'watch']);
